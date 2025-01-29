f1ef106c2bb62b722c314c6e867472e0091b2269
Kimomwe2003 <lidyakimomwe@gmail.com>
2025-01-29 21:45:43 +0300

    creating superuser 3

--- build.sh
+++ build.sh
@@ -12,7 +12,8 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 
-if[[$CREATE_SUPERUSER]];
+if [[ $CREATE_SUPERUSER ]];
 then
-  python manage.py createsuperuser --no-input
+  python myschoolproject/manage.py createsuperuser --no-input
 fi
+
